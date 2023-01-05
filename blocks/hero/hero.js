de7df9535fd7fbd2f60718b25f380a070fae1194
Sean Steimer <ssteimer@adobe.com>
2023-01-04 17:04:46 -0800

    fix: eslint ignore vars beginning with _

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,6 +2,6 @@
  * decorates the hero
  * @param {Element} block The hero block element
  */
-export default async function decorate(block) {
+export default async function decorate(_block) {
   // no-op for now
 }
