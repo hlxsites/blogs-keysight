8ee621474a2628f784c0f2d65ef72c829a95b0c7
Brian Warner <3vil3mpir3@gmail.com>
2023-01-09 09:53:32 -0500

    rerunning lint

--- templates/author/author.js
+++ templates/author/author.js
@@ -3,7 +3,7 @@ import { decorateIcons } from '../../scripts/lib-franklin.js';
 export default function decorate(doc) {
   doc.querySelector('ul').querySelectorAll('a').forEach((a) => {
     a.innerHTML = '';
-    const social = (new URL(a)).hostname.replace('www.', '').replace('.com', ''); // get social domain name.
+    const social = (new URL(a)).hostname.replace('www.', '').replace('.com', ''); // get social domain name
     a.insertAdjacentHTML('beforeend', `<span class="icon icon-${social}"></span>`);
     decorateIcons(a);
   });
