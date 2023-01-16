4684a6f2c9cd33ed9bdbbd265e5c79b45650e015
Brian Warner <3vil3mpir3@gmail.com>
2023-01-16 10:23:35 -0500

    CSS and JS changes to account for re-gridding blog page

--- templates/post/post.js
+++ templates/post/post.js
@@ -1,7 +1,19 @@
 import { getMetadata, createOptimizedPicture, decorateIcons } from '../../scripts/lib-franklin.js';
 import { createElement, getPosts } from '../../scripts/scripts.js';
 
-async function buildAuthorImage(sidebar, author) {
+const socialIcons = ['facebook', 'twitter', 'linkedin', 'email'];
+
+function buildSocial(sidebar) {
+  const social = createElement('div', 'social');
+  for (let i = 0; i < socialIcons.length; i += 1) {
+    const classes = ['icon', `icon-${socialIcons[i]}`];
+    const socialIcon = createElement('span', classes);
+    social.appendChild(socialIcon);
+  }
+  sidebar.append(social);
+}
+
+async function buildSidebar(sidebar, author) {
   let authorImage;
   let authorName;
   let authorTitle;
@@ -18,18 +30,11 @@ async function buildAuthorImage(sidebar, author) {
   const picMedia = [{ media: '(min-width: 160px)', width: '160' }];
   const pic = createOptimizedPicture(authorImage, '', false, picMedia);
   sidebar.innerHTML = `<a class="author-image" href="${authorUrl}">${pic.outerHTML}</a>
-  <div class="author-details">
-  <h3 class="author-name"><a href="${authorUrl}">${authorName}</a></h3>
-  <h4 class="author-title">${authorTitle}</h4>
-  </div>
-  <div class="social">
-  <span class="icon icon-facebook"></span>
-  <span class="icon icon-twitter"></span>
-  <span class="icon icon-linkedin"></span>
-  <span class="icon icon-email"></span>
-  </div>
-
-    `;
+    <div class="author-details">
+    <h3 class="author-name"><a href="${authorUrl}">${authorName}</a></h3>
+    <h4 class="author-title">${authorTitle}</h4>
+    </div>`;
+  buildSocial(sidebar);
   decorateIcons(sidebar);
 }
 
@@ -38,16 +43,16 @@ function buildPostData(contentarea, sidebar, contentcontainer) {
   const pubdate = getMetadata('publication-date');
   const readtime = getMetadata('read-time');
   contentcontainer.insertAdjacentHTML('afterbegin', `<p class='blog-category'><a href='#'>${topic}</a></p>`);
-  contentcontainer.querySelector('h1').insertAdjacentHTML('afterend', `<div class='post-stats'><span class='pubdate'>${pubdate}</span>  | <span class="icon icon-clock"></span><span class='readtime'>${readtime}</span></div>`);
+  contentcontainer.querySelector('h1').insertAdjacentHTML('afterend', `<div class='post-stats'><span class='pubdate'>${pubdate}</span> | <span class="icon icon-clock"></span><span class='readtime'>${readtime}</span></div>`);
   contentarea.parentNode.insertBefore(sidebar, contentarea);
-  decorateIcons(contentarea);
+  decorateIcons(contentcontainer);
 }
 
 export default function decorate(doc) {
-  const contentarea = doc.querySelector('.hero-container').nextElementSibling;
-  const contentcontainer = contentarea.firstElementChild;
-  const classes = ['post-sidebar'];
+  const contentarea = doc.querySelector('.post-cards-container').previousElementSibling.classList.contains('related-content-container') ? doc.querySelector('.post-cards-container').previousElementSibling : doc.querySelector('.post-cards-container').previousElementSibling.previousElementSibling;
+  const contentcontainer = doc.querySelector('.hero-container').nextElementSibling.firstElementChild;
+  const classes = ['section', 'post-sidebar'];
   const sidebar = createElement('div', classes);
-  buildAuthorImage(sidebar, getMetadata('author'));
+  buildSidebar(sidebar, getMetadata('author'));
   buildPostData(contentarea, sidebar, contentcontainer);
 }
