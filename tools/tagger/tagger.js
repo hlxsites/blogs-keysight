69d9b0ca210332ee21bf825783b29bd167ce8d7d
Sean Steimer <ssteimer@adobe.com>
2023-01-18 11:30:38 -0800

    chore: tagger

--- tools/tagger/tagger.js
+++ tools/tagger/tagger.js
@@ -1,8 +1,21 @@
-let taxonomy;
-const selected = [];
-
-function initTaxonomy(tax) {
-  taxonomy = tax;
+function initTaxonomy(taxonomy) {
+  let html = '';
+  Object.values(taxonomy).forEach((cat, idx) => {
+    html += '<div class="category">';
+    html += `<h2>${cat.title}</h2>`;
+    const items = cat.children;
+    items.forEach((tag) => {
+      const { title, path } = tag;
+      html += `
+        <span data-target="${title}" data-category="${cat.title}" class="path">${path}
+          <span class="tag cat-${idx}">${title}</span>
+        </span>
+      `;
+    });
+    html += '</div>';
+  });
+  const results = document.getElementById('results');
+  results.innerHTML = html;
 }
 
 async function getTaxonomy() {
@@ -12,77 +25,92 @@ async function getTaxonomy() {
   div.innerHTML = markup;
   const level1 = div.querySelector('ul').querySelectorAll(':scope > li');
 
-  const mapChildren = (li) => {
+  const mapChildren = (li, parentPath) => {
     const title = li.childNodes[0].textContent.trim();
     const childrenLis = li.querySelectorAll(':scope > ul > li');
+    const path = `${parentPath}${title}`;
     return {
       title,
-      children: [...childrenLis].map(mapChildren),
+      path: parentPath,
+      children: [...childrenLis].map((childLi) => mapChildren(childLi, `${path}<span class="psep"> / </span>`)),
     };
   };
 
-  const data = [...level1].map(mapChildren);
+  const data = [...level1].map((li) => mapChildren(li, ''));
   return data;
 }
 
 function filter() {
   const searchTerm = document.getElementById('search').value.toLowerCase();
-  let html = '';
-
-  Object.values(taxonomy).forEach((cat, idx) => {
-    html += '<div class="category">';
-    html += `<h2>${cat.title}</h2>`;
-    const items = cat.children;
-    items.forEach((tag) => {
-      const { title } = tag;
-      const offset = title.toLowerCase().indexOf(searchTerm);
-      if (offset >= 0) {
-        const before = title.substr(0, offset);
-        const term = title.substr(offset, searchTerm.length);
-        const after = title.substr(offset + searchTerm.length);
-        html += `
-          <span data-target="${title}" data-category="${cat.title}" class="path">
-            <span class="tag cat-${idx}">${before}<span class="highlight">${term}</span>${after}</span>
-          </span>
-        `;
-      }
-    });
-    html += '</div>';
+  document.querySelectorAll('#results .tag').forEach((tag) => {
+    const title = tag.textContent.trim();
+    const offset = title.toLowerCase().indexOf(searchTerm);
+    if (offset >= 0) {
+      const before = title.substr(0, offset);
+      const term = title.substr(offset, searchTerm.length);
+      const after = title.substr(offset + searchTerm.length);
+      tag.innerHTML = `${before}<span class="highlight">${term}</span>${after}`;
+      tag.closest('.path').classList.remove('filtered');
+    } else {
+      tag.closest('.path').classList.add('filtered');
+    }
   });
-  const results = document.getElementById('results');
-  results.innerHTML = html;
+}
+
+function toggleTag(target) {
+  target.classList.toggle('selected');
+  // eslint-disable-next-line no-use-before-define
+  displaySelected();
 }
 
 function displaySelected() {
+  const selEl = document.getElementById('selected');
+  const toCopyBuffer = [];
 
-}
+  selEl.innerHTML = '';
+  const selectedTags = document.querySelectorAll('#results .path.selected');
+  if (selectedTags.length > 0) {
+    selectedTags.forEach((tag) => {
+      const clone = tag.cloneNode(true);
+      clone.classList.remove('filtered', 'selected');
+      clone.addEventListener('click', () => {
+        toggleTag(tag);
+      });
+      toCopyBuffer.push(clone.querySelector('.tag').textContent);
+      selEl.append(clone);
+    });
+    const button = document.createElement('button');
+    button.addEventListener('click', () => {
+      const copyText = document.getElementById('copybuffer');
 
-function handlePaste(_event) {
+      copyText.select();
+      copyText.setSelectionRange(0, 99999);
 
-}
+      document.execCommand('copy');
 
-function addTag(event) {
-  const target = event.getAttribute('data-target');
-  const cat = event.getAttribute('data-category');
-  selected.push({
-    name: target,
-    category: cat,
-  });
-  displaySelected();
+      button.disabled = true;
+    });
+    button.textContent = 'Copy';
+    selEl.append(button);
+    selEl.classList.remove('hidden');
+  } else {
+    selEl.classList.add('hidden');
+  }
+
+  const copybuffer = document.getElementById('copybuffer');
+  copybuffer.value = toCopyBuffer.join(', ');
 }
 
 async function init() {
   const tax = await getTaxonomy();
 
   initTaxonomy(tax);
-  filter();
 
-  document.addEventListener('paste', handlePaste);
   document.querySelector('#search').addEventListener('keyup', filter);
   document.addEventListener('click', (e) => {
     const target = e.target.closest('.category .path');
     if (target) {
-      addTag(e);
+      toggleTag(target);
     }
   });
 }
