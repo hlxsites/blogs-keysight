47cc2c0648471907d42a5cfdf10c0bc105fd3fe4
Sean Steimer <ssteimer@adobe.com>
2023-01-20 15:21:04 -0800

    chore: related content block

---
+++ blocks/related-content/related-content.js
@@ -0,0 +1,19 @@
+import {
+  createElement,
+} from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const colWrapper = block.firstElementChild;
+  colWrapper.classList.add('cols');
+  const cols = [...colWrapper.children];
+  cols.forEach((col) => {
+    const list = createElement('ul');
+    col.querySelectorAll('a').forEach((a) => {
+      const li = createElement('li');
+      a.classList.remove('button');
+      li.append(a);
+      list.append(li);
+    });
+    col.append(list);
+  });
+}
