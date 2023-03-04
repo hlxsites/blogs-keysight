748abb97633cd2a8c170afd4466c90da9d4fe687
Sean Steimer <ssteimer@adobe.com>
2023-03-04 11:20:52 -0800

    no buttons in image blocks

--- blogs/blocks/image/image.js
+++ blogs/blocks/image/image.js
@@ -2,4 +2,6 @@ export default async function decorate(block) {
   const pic = block.querySelector('picture');
   const container = pic.closest('div');
   container.classList.add('container');
+
+  block.querySelectorAll('a.button').forEach((button) => button.classList.remove('button'));
 }
