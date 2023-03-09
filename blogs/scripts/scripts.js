3f09a82ae5f7a7ecd4ccfbbe68c5d676449ac463
Sean Steimer <ssteimer@adobe.com>
2023-03-09 00:13:49 -0800

    more delay

--- blogs/scripts/scripts.js
+++ blogs/scripts/scripts.js
@@ -523,7 +523,7 @@ function loadDelayed() {
   window.setTimeout(() => {
     // eslint-disable-next-line import/no-cycle
     import('./delayed.js');
-  }, 3000);
+  }, 4000);
   // load anything that can be postponed to the latest here
 }
 
