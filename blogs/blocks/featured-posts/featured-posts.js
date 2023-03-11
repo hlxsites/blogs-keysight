d5379dd6e75904962b4ed28423fd923948da4243
Sean Steimer <ssteimer@adobe.com>
2023-03-10 23:48:13 -0800

    testing speed

--- blogs/blocks/featured-posts/featured-posts.js
+++ blogs/blocks/featured-posts/featured-posts.js
@@ -23,61 +23,64 @@ export default async function decorate(block) {
         </div>
     `;
 
-    if (link.title !== 'auto') {
-      fetch(`${link.href}.plain.html`).then((resp) => {
-        if (resp.ok) {
-          resp.text().then((html) => {
-            const temp = createElement('div');
-            temp.innerHTML = html;
-            const image = temp.querySelector('img');
-            const title = temp.querySelector('h1');
-            if (image) {
-              const pic = createOptimizedPicture(image.src, '', false, [{ width: '200' }]);
-              post.querySelector('.picture-placeholder').replaceWith(pic);
-            }
-            post.querySelector(':scope > div > a').textContent = title.textContent;
-            post.classList.remove('post-placeholder');
-          });
-        } else {
-          post.remove();
-        }
-      });
-    } else {
-      fetch('/blogs/query-index.json?limit=500').then((resp) => {
-        if (resp.ok) {
-          resp.json().then((json) => {
-            const topic = getMetadata('topic');
-            const subTopic = getMetadata('subtopic');
-            const mostRecentPost = json.data.find((postData) => {
-              if (!topic) {
-                // no topic so just get the first one
-                return true;
+    // small delay to get to lcp faster
+    setTimeout(() => {
+      if (link.title !== 'auto') {
+        fetch(`${link.href}.plain.html`).then((resp) => {
+          if (resp.ok) {
+            resp.text().then((html) => {
+              const temp = createElement('div');
+              temp.innerHTML = html;
+              const image = temp.querySelector('img');
+              const title = temp.querySelector('h1');
+              if (image) {
+                const pic = createOptimizedPicture(image.src, '', false, [{ width: '200' }]);
+                post.querySelector('.picture-placeholder').replaceWith(pic);
               }
-
-              if (topic === postData.topic) {
-                if (subTopic) {
-                  return subTopic === postData.subtopic;
-                }
-                // topic match but no subtopic
-                return true;
-              }
-              return false;
-            });
-            if (mostRecentPost) {
-              const pic = createOptimizedPicture(mostRecentPost.image, '', false, [{ width: '200' }]);
-              post.innerHTML = `
-              <a href="${mostRecentPost.path}" title="${mostRecentPost.title.replaceAll('"', '')}">${pic.outerHTML}</a>
-              <div>
-                <p class="category">${heading.textContent}</p>
-                <a href="${mostRecentPost.path}">${mostRecentPost.title}</a>
-              </div>`;
+              post.querySelector(':scope > div > a').textContent = title.textContent;
               post.classList.remove('post-placeholder');
-            } else {
-              post.remove();
-            }
-          });
-        }
-      });
-    }
+            });
+          } else {
+            post.remove();
+          }
+        });
+      } else {
+        fetch('/blogs/query-index.json?limit=500').then((resp) => {
+          if (resp.ok) {
+            resp.json().then((json) => {
+              const topic = getMetadata('topic');
+              const subTopic = getMetadata('subtopic');
+              const mostRecentPost = json.data.find((postData) => {
+                if (!topic) {
+                  // no topic so just get the first one
+                  return true;
+                }
+  
+                if (topic === postData.topic) {
+                  if (subTopic) {
+                    return subTopic === postData.subtopic;
+                  }
+                  // topic match but no subtopic
+                  return true;
+                }
+                return false;
+              });
+              if (mostRecentPost) {
+                const pic = createOptimizedPicture(mostRecentPost.image, '', false, [{ width: '200' }]);
+                post.innerHTML = `
+                <a href="${mostRecentPost.path}" title="${mostRecentPost.title.replaceAll('"', '')}">${pic.outerHTML}</a>
+                <div>
+                  <p class="category">${heading.textContent}</p>
+                  <a href="${mostRecentPost.path}">${mostRecentPost.title}</a>
+                </div>`;
+                post.classList.remove('post-placeholder');
+              } else {
+                post.remove();
+              }
+            });
+          }
+        });
+      }
+    }, 250);
   });
 }
