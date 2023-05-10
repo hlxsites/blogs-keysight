5deb57b10bb2fd989e5ae2dc79308db7b72f3420
Rob Rusher <robrusher@gmail.com>
2023-05-10 14:59:40 -0600

    adjust feed location

--- .github/ci/generate-news-feed.js
+++ .github/ci/generate-news-feed.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 const endpoint = 'https://www.keysight.com/blogs/query-index.json';
 const feedInfoEndpoint = 'https://www.keysight.com/blogs/feed-info.json';
-const targetDirectory = '../../rss';
+const targetDirectory = '../../blogs/rss';
 const targetFile = `${targetDirectory}/feed.xml`;
 const limit = 1000;
 
