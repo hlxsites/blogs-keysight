8d94b3e530db9cb24089768f4183243dd4ecd901
Rob Rusher <robrusher@gmail.com>
2023-05-22 16:31:32 -0600

    clean up linting

--- blogs/scripts/taxonomy.js
+++ blogs/scripts/taxonomy.js
@@ -3,14 +3,14 @@ import { toClassName } from './lib-franklin.js';
 async function getTags(ul) {
   const tagList = ul.querySelectorAll('ul li ul li');
   const tagArray = [...tagList];
-  const textArray = tagArray.map(li => toClassName(li.textContent));
+  const textArray = tagArray.map((li) => toClassName(li.textContent));
   return textArray;
 }
 
-export async function getTaxonomy() {
+export default async function getTaxonomy() {
   let resp;
   try {
-    // Fetch the HTML content of the webpage  
+    // Fetch the HTML content of the webpage
     resp = await fetch(`${origin}/blogs/tags.plain.html`);
     if (resp && resp.ok) {
       const text = await resp.text();
@@ -20,12 +20,10 @@ export async function getTaxonomy() {
       const rootUlElement = tempElement.querySelector('ul');
       // Create the JavaScript object from the nested <ul>
       const result = getTags(rootUlElement);
-
-      console.log(result);
       return result;
     }
-  }
-  catch {
+  } catch {
     // fail
   }
+  return null;
 }
