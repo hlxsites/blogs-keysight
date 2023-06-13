7a645954fb7d6f9e669e4d3ad263f1596d9da680
dylandepass <dylandepass@gmail.com>
2023-06-13 12:12:14 -0400

    Update tags.js

--- blogs/blocks/tags/tags.js
+++ blogs/blocks/tags/tags.js
@@ -6,7 +6,6 @@ import {
   filterPosts,
 } from '../../scripts/scripts.js';
 import { getOrigin, readBlockConfig } from '../../scripts/lib-franklin.js';
-import { readBlockConfig } from '../../scripts/lib-franklin.js';
 import { validateTagObjs } from '../../scripts/taxonomy.js';
 
 function buildSearch(block) {
