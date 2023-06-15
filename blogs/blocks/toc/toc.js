cea65db5d0d1f3ee4a3bd765283c5e887bfd0fcb
Sean Steimer <ssteimer@adobe.com>
2023-06-15 11:04:39 -0700

    fix: toc

--- blogs/blocks/toc/toc.js
+++ blogs/blocks/toc/toc.js
@@ -4,7 +4,13 @@ export default function decorate(block) {
   const ul = createElement('ul', 'toc-list');
   block.innerHTML = '';
   block.append(ul);
-  document.querySelectorAll('main h2').forEach((h2) => {
+
+  const h2s = [...document.querySelectorAll('main h2')];
+  const h3s = [...document.querySelectorAll('main h3')];
+
+  let h3Idx = 0;
+  for (let i = 0; i < h2s.length; i += 1) {
+    const h2 = h2s[i];
     const li = createElement('li', 'toc-h2');
     const a = createElement('a');
     a.href = `#${h2.id}`;
@@ -12,17 +18,25 @@ export default function decorate(block) {
     li.append(a);
     ul.append(li);
 
-    const h3s = h2.querySelectorAll('h3');
-    if (h3s.length > 0) {
-      const subUl = createElement('ul', 'toc-list');
-      li.append(subUl);
-      h3s.forEach((h3) => {
-        const subLi = createElement('li', 'toc-h3');
-        const subA = createElement('a');
-        subA.href = `#${h3.id}`;
-        subA.textContent = h2.textContent;
-        subLi.append(subA);
-      });
+    const subUl = createElement('ul', ['toc-sub-list', 'toc-list-empty']);
+    li.append(subUl);
+    const nextH2 = h2s[i + 1];
+    for (let j = h3Idx; j < h3s.length; j += 1) {
+      const h3 = h3s[j];
+      // eslint-disable-next-line no-bitwise
+      if (h2.compareDocumentPosition(h3) & Node.DOCUMENT_POSITION_FOLLOWING) {
+        // eslint-disable-next-line no-bitwise
+        if (!nextH2 || (nextH2.compareDocumentPosition(h3) & Node.DOCUMENT_POSITION_PRECEDING)) {
+          const subLi = createElement('li', 'toc-h3');
+          const subA = createElement('a');
+          subA.href = `#${h3.id}`;
+          subA.textContent = h3.textContent;
+          subLi.append(subA);
+          subUl.append(subLi);
+          subUl.classList.remove('toc-list-empty');
+        }
+      }
+      h3Idx += 1;
     }
-  });
+  }
 }
