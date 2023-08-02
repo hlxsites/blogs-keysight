c58c74ce82f62d20c8b29ee7b19388ba8055efbb
Sean Steimer <ssteimer@adobe.com>
2023-08-02 06:30:41 -0700

    cta adjustments for image

--- blogs/blocks/cta/cta.js
+++ blogs/blocks/cta/cta.js
@@ -1,3 +1,11 @@
-export default function decorate(_block) {
-  // no op
+export default function decorate(block) {
+  [...block.firstElementChild.children].forEach((col) => {
+    const pic = col.querySelector('picture');
+    if (pic) {
+      block.classList.add('cta-has-image');
+      col.classList.add('cta-img-container');
+    } else {
+      col.classList.add('cta-text-container');
+    }
+  });
 }
